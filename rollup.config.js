2734383f4c43e14ba0348b3b3929fee968917a27
Ebuka Odini <ebukaodini@gmail.com>
2024-11-23 22:01:40 +0100

    refactor: updated template

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
-import {
-  chromeExtension,
-  simpleReloader,
-} from 'rollup-plugin-chrome-extension'
+import { chromeExtension } from 'rollup-plugin-chrome-extension'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
@@ -16,7 +13,6 @@ export default {
   },
   plugins: [
     chromeExtension(),
-    simpleReloader(),
     typescript(),
     resolve(),
     commonjs(),
